a739029fa9012e2d25392dbffe1db9f006a9c991
ArpitaVBamhane <arpitabamhane_iot_2020@ltce.in>
2023-09-10 07:14:10 -0400

    added a mul program

---
+++ mul.java
@@ -0,0 +1,12 @@
+class mul
+{
+public static void main(String[] args)
+{
+int i;
+int num = Integer.parseInt(args[0]);
+for(i = 1; i <= 10; i++)
+{
+System.out.println(num+ " * " +i+ " = " +num * i);
+}
+}
+}
